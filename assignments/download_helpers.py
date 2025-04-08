9c07a9e405ead0c98cae48f5fb0d2421e71de1b9
mcoffman1 <100247620+mcoffman1@users.noreply.github.com>
2025-04-08 13:41:24 -0500

    Update download_helpers.py
    use my github

--- assignments/download_helpers.py
+++ assignments/download_helpers.py
@@ -6,6 +6,6 @@ if Path("helper_functions.py").is_file():
   print("helper_functions.py already exists")
 else:
   print("Downloading helper_functions.py")
-  request = requests.get("https://raw.githubusercontent.com/mrdbourke/pytorch-deep-learning/main/helper_functions.py")
+  request = requests.get("https://raw.githubusercontent.com/mcoffman1/ROB-144/refs/heads/main/assignments/helper_functions.py")
   with open("helper_functions.py", "wb") as f:
     f.write(request.content)
